d8f859223c02aac196196020cb1f59465462ff86
Cédric Bonhomme <cedric@cedricbonhomme.org>
2016-11-22 08:25:31 +0100

    Added manager.py

---
+++ manager.py
@@ -0,0 +1,22 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from flask_script import Manager
+
+API_LOGIN = ""
+API_PASSWD = ""
+
+manager = Manager()
+
+
+@manager.command
+def fetch(limit=100, retreive_all=False):
+    "Crawl the feeds with the client crawler."
+    from http_crawler import CrawlerScheduler
+    scheduler = CrawlerScheduler(API_LOGIN, API_PASSWD)
+    scheduler.run(limit=limit, retreive_all=retreive_all)
+    scheduler.wait()
+
+
+if __name__ == '__main__':
+    manager.run()
